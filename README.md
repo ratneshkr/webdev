db41aca50ea83876b205eec3f522c31eefeac4f1
Ratnesh <parallelratnesh@gmail.com>
2021-06-30 18:59:00 +0545

    day2

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # webdev
+This is a training program run by techpointer learning hub
---
+++ calculator.js
@@ -0,0 +1,27 @@
+function add() {
+    var name1 = parseInt(document.getElementById("name1").value);
+    var name2 = parseInt(document.getElementById("name2").value);
+
+    var result = name1 + name2;
+    alert(result);
+}
+
+
+
+function sub() {
+    var name1 = parseInt(document.getElementById("name1").value);
+    var name2 = parseInt(document.getElementById("name2").value);
+
+    var result = name1 - name2;
+    alert(result);
+}
+
+
+function mul() {
+    var name1 = parseInt(document.getElementById("name1").value);
+    var name2 = parseInt(document.getElementById("name2").value);
+
+    var result = name1 * name2;
+    alert(result);
+}
+
